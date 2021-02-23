e706f9d3510fd0bcf4d4302901b22b205344c6ab
Keven Santana <kevensantanta40@gmail.com>
2021-02-23 20:02:18 -0300

    Desvendando Next.js

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/kevensantana.png" alt="Keven Santana"/>
+            <div>
+                <strong>Keven Santana</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                
+                </p>
+            </div>
+        </div>
+    );
+}
